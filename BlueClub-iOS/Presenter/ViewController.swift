08dddf844f74d507321ed34bf9e2353277e9872f
insub <vb15@kakao.com>
2024-01-03 23:10:19 +0900

    initial

---
+++ BlueClub-iOS/Presenter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BlueClub-iOS
+//
+//  Created by 김인섭 on 1/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .gray
+    }
+
+
+}
+
